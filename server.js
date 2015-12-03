3cb9afc58b1d2c834ccffddf06ff8ba561d173eb
Tiago2R <tiago.rr@outlook.com>
2015-12-03 14:31:00 -0500

    Setup dependencies and connect to mongo (MongoLab)

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+// server.js
+var port = process.env.PORT || 8080;
+
+// load dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.json());
+
+// setup db connection
+mongoose.set('debug', true);
+mongoose.connect('mongodb://user:test@ds063124.mongolab.com:63124/mongo_test');
+mongoose.connection.on('error', function(err){console.log(err);});
+
+// router
+var router = express.Router();
+                       
+// launch application
+app.listen(port);
+
 console.log("Hello World!")
